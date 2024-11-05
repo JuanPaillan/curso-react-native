1c94901538c758062ef40430218f5b5dd652a8a7
Juan Paillan <juanedgardo.paillan@alumnos.ulagos.cl>
2024-11-05 12:06:27 -0300

    se modifico el color de degradez y el texto

--- app1/App.js
+++ app1/App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient'; //se importa el lineargradient
 
 export default function App() {
   return (
     <LinearGradient
-      colors={['#f58723', '#fbbb0c', '#0b110d']} // Colores del gradiente
+      colors={['#f58723', '#fbbb0c', '#93a31c']} //colores para el degradez
       style={styles.container}
     >
-      <Text style={styles.text}>¡Texto con gradiente de fondo!</Text>
+      <Text style={styles.text}>Texto con GRADIANTE de fondo</Text>
       <StatusBar style="auto" />
     </LinearGradient>
   );
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 20,
-    fontWeight: '600', // Semi-negrita
-    color: 'green',    // Color verde
+    fontWeight: '600',
+    color: 'green',   
   }
 });
