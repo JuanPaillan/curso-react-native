ebda125682a944301c28c0ddbcaece8a869fc1f8
Juan Paillan <juanedgardo.paillan@alumnos.ulagos.cl>
2024-11-05 16:07:37 -0300

    se agregaron comentarios al app.js

--- appnavegacion2/App.js
+++ appnavegacion2/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { StyleSheet, Text, View, Image } from 'react-native'; //se agrega la importacion de imagenes
+import { LinearGradient } from 'expo-linear-gradient'; //se importa el lineargradient
+import Icon from 'react-native-vector-icons/FontAwesome'; //se importa el componente de iconos
 
-export default function AlbumScreen() {
+export default function App() {
   return (
-    <LinearGradient colors={['#fd190c', '#d6aa26', '#0a0c0d']} style={styles.container}>
+    //fondo degradado con lineargradient
+    <LinearGradient colors={['#d2232b', '#7d222a', '#0a0c0d']} style={styles.container}>
       <View style={styles.albumCoverContainer}>
         <Image
-          source={{ uri: 'https://cdns-images.dzcdn.net/images/cover/69614595335e77b6600283182a24ea10/0x1900-000000-80-0-0.jpg' }} // Cambia esto por la URL de tu imagen de portada
+          //imagen de la cancion
+          source={{ uri: 'https://images.genius.com/e5cfbe8cfba6296b28ab86576df41f99.1000x1000x1.png' }} 
           style={styles.albumCover}
         />
-        <View style={styles.textContainer}>
-          <Text style={styles.songTitle}>Tu Perfume A Waren</Text>
-          <Text style={styles.artistName}>JB The Voice</Text>
-        </View>
+        <View style={styles.textContainer}> 
+          <Text style={styles.MainText}>Vea Pues</Text> 
+          <Text style={styles.SubText}>Easykid</Text>
+        </View> 
       </View>
       
       <View style={styles.iconContainer}>
-        <Icon name="heart" size={30} color="#fff" style={styles.icon} />
+        <Icon name="heart" size={30} color="#fff" style={styles.icon} /> 
       </View>
       
       <View style={styles.controlsContainer}>
@@ -49,18 +51,18 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 5,
-    elevation: 5, // Para Android
+    elevation: 5, 
   },
   textContainer: {
     alignItems: 'center',
     marginTop: 10,
   },
-  songTitle: {
+  MainText: {
     fontSize: 20,
     fontWeight: 'bold',
     color: '#fff',
   },
-  artistName: {
+  SubText: {
     fontSize: 15,
     color: '#fff',
   },
