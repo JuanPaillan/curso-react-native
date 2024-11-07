403f711a373777246cae58c45b374ebfb80e5d1a
Juan Paillan <juanedgardo.paillan@alumnos.ulagos.cl>
2024-11-06 23:36:33 -0300

    se modifico el icono heart y el sombreado de la imagen

--- appnavegacion2/App.js
+++ appnavegacion2/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native'; //se agrega la importacion de imagenes
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'; //se agrega la importacion de imagenes
 import { LinearGradient } from 'expo-linear-gradient'; //se importa el lineargradient
 import Icon from 'react-native-vector-icons/FontAwesome'; //se importa el componente de iconos
 
 export default function App() {
+
+  const [Isliked, Setisliked] = useState(false);
+  const toggleheart = () => {
+    Setisliked(!Isliked);
+  };
   return (
-    //fondo degradado con lineargradient
+    //fondo degradado con lineargradient 
     <LinearGradient colors={['#d2232b', '#7d222a', '#0a0c0d']} style={styles.container}>
       <View style={styles.albumCoverContainer}>
-        <Image
-          //imagen de la cancion
+        <View style={styles.shadowContainer}>
+          <Image //imagen de la portada
           source={{ uri: 'https://images.genius.com/e5cfbe8cfba6296b28ab86576df41f99.1000x1000x1.png' }} 
           style={styles.albumCover}
-        />
+          />
+        </View>
         <View style={styles.textContainer}> 
           <Text style={styles.MainText}>Vea Pues</Text> 
           <Text style={styles.SubText}>Easykid</Text>
         </View> 
       </View>
       
-      <View style={styles.iconContainer}>
-        <Icon name="heart" size={30} color="#fff" style={styles.icon} /> 
-      </View>
+      <TouchableOpacity onPress={toggleheart} style={styles.iconContainer}>
+        <Icon
+          name="heart"
+          size={30}
+          color={Isliked ? "red" : "#fff"} //cambia de color segun el estado
+          style={styles.icon}
+        />
+      </TouchableOpacity>
       
       <View style={styles.controlsContainer}>
         <Icon name="backward" size={30} color="#fff" style={styles.controlIcon} />
@@ -43,16 +54,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 20,
   },
-  albumCover: {
+  shadowContainer: {
     width: 250,
     height: 250,
     borderRadius: 15,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 5,
-    elevation: 5, 
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    elevation: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  albumCover: {
+    width: 240,
+    height: 240,
+    borderRadius: 15,
+  },
+  
   textContainer: {
     alignItems: 'center',
     marginTop: 10,
